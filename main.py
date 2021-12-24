b40109734a3a8c67a0e34abe58bc3ae3f346cdd5
Andrei Klepatch <56235713+HighlyTrainedNeuralNetwork@users.noreply.github.com>
2021-12-23 20:49:57 -0500

    Refactored completely.

--- main.py
+++ main.py
@@ -7,42 +7,30 @@ load_dotenv('.env')
 intents = discord.Intents().all()
 client = commands.Bot(command_prefix= "!", intents = intents)
 important_message_id = "None"
-vote_initiated = False
-@client.event
-# This has no use apart from being a troubleshooting tool.
-async def on_ready():
-    print("Bot online")
+vote_dict = {}
 
 @client.command()
 async def nominate(ctx):
-    global vote_initiated
-    if vote_initiated == False:
-        vote_initiated = True
-        global voted_yes
-        voted_yes = []
-        global voted_no
-        voted_no = []
-        global member_amount
-        member_amount = len(ctx.message.guild.members) - 1
-        proposition = ctx.message.content.split("!nominate ")[-1]
-        await ctx.message.channel.send("%s has been nominated. React to this message with 👍 or 👎 to vote." % proposition)
-        # author__name here be whatever username you assign to your bot when creating it.
-        message = await ctx.message.channel.history().get(author__name='Anonymous Voting Bot')
-        global important_message_id
-        important_message_id = message.id
+    proposition = ctx.message.content.split("!nominate ")[-1]
+    if proposition in [vote_dict[key][0] for key in vote_dict.keys()]:
+        await ctx.message.channel.send("This proposition has already been nominated.")
     else:
-        #TODO add capability to manage multiple active propositions.
-        await ctx.message.channel.send("You cannot nominate a proposition until the current nomination has been resolved.")
+        await ctx.message.channel.send("%s has been nominated. React to this message with 👍 or 👎 to vote. 0 people have"
+                                       " voted aye and 0 people have voted nay." % proposition)
+        message = await ctx.message.channel.history().get(author__name='Anonymous Voting Bot')
+        vote_dict[message.id] = [proposition, 0, 0, []]
 
 @client.command()
 async def dismiss(ctx):
-    global vote_initiated
-    if vote_initiated == True:
-        await ctx.message.channel.send("You have successfully dismissed the current nomination.")
+    message = ctx.message.content.split("!dismiss ")[-1]
+    if message in [vote_dict[key][0] for key in vote_dict.keys()]:
+        for key in vote_dict.keys():
+            if vote_dict[key][0] == message:
+                await ctx.message.channel.send("You have successfully dismissed the nomination for %s." % message)
+                del vote_dict[key]
+                break
     else:
-        await ctx.message.channel.send("No nomination to be dismissed.")
-    vote_initiated = False
-
+        await ctx.message.channel.send("No such nomination to be dismissed.")
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -50,24 +38,17 @@ async def on_raw_reaction_add(payload):
     reaction_message_id = payload.message_id
     message = await channel.fetch_message(reaction_message_id)
     user = client.get_user(payload.user_id)
-    global voted_yes
-    global voted_no
-    emoji = payload.emoji
-    if important_message_id != "None":
-        if reaction_message_id == important_message_id and (user not in voted_yes) and (user not in voted_no):
-            # The two emoji defined here can be anything as long as one is associated with each possible vote.
-            if str(emoji) == "👍":
-                voted_yes.append(user)
-            elif str(emoji) == "👎":
-                voted_no.append(user)
-            if len(voted_yes) + len(voted_no) == member_amount:
-                global vote_initiated
-                vote_initiated = False
-                # 0.75 of the full member list is an arbitrary conditon for resolving the vote. Other conditions connected to time or amount of votes can easily be substituted.
-                if len(voted_yes) / member_amount >= 0.75:
-                    await channel.send("This proposition passed.")
-                else:
-                    await channel.send("This proposition did not pass.")
-    await message.remove_reaction(emoji, user)
-# This uses a Discord generated when registering an application through the Discord Developer Portal (https://discord.com/developers/applications).
+    reaction = payload.emoji
+    if reaction_message_id in vote_dict.keys():
+        if user not in vote_dict[reaction_message_id][3]:
+            vote_dict[reaction_message_id][3].append(user)
+            if str(reaction) == "👍":
+                vote_dict[reaction_message_id][1] += 1
+            elif str(reaction) == "👎":
+                vote_dict[reaction_message_id][2] += 1
+        await message.remove_reaction(reaction, user)
+        await message.edit(content="%s has been nominated. React to this message with 👍 or 👎 to vote. %d people have"
+                                            " voted aye and %d people have voted nay." % (vote_dict[reaction_message_id][0],
+                                            vote_dict[reaction_message_id][1], vote_dict[reaction_message_id][2]))
+
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
